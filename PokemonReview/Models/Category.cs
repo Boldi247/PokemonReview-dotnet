a9154ddc5d0bb5487857df0b302b4d32732a651d
Boldi247 <kovacs.boldizsar0321@gmail.com>
2024-06-04 22:36:20 +0200

    created models

---
+++ PokemonReview/Models/Category.cs
@@ -0,0 +1,8 @@
+namespace PokemonReview.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<PokemonCategory> PokemonCategories { get; set; }
+}
---
+++ PokemonReview/Models/Review.cs
@@ -0,0 +1,10 @@
+namespace PokemonReview.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public Reviewer Reviewer { get; set; }
+    public Pokemon Pokemon { get; set; }
+}
